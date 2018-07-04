f93f1f38f2593334e35b50b5cfa45764a7b75dc6
z1max <forcazimaxqwe123>
2018-07-04 16:20:12 +0300

    Add entities

---
+++ src/main/java/by/z1max/model/Genre.java
@@ -0,0 +1,21 @@
+package by.z1max.model;
+
+public enum Genre {
+    ACTION,
+    ADVENTURE,
+    SCI_FI,
+    COMEDY,
+    DRAMA,
+    ROMANCE,
+    CRIME,
+    FANTASY,
+    THRILLER,
+    WAR,
+    HORROR,
+    MYSTERY,
+    ANIMATION,
+    HISTORY,
+    BIOGRAPHY,
+    SPORT,
+    WESTERN
+}
