a788575ad84263d9e8cff25364f71dd055b17b67
z1max <forcazimaxqwe123>
2018-07-09 18:02:24 +0300

    Remove dao exception

--- src/main/java/by/z1max/service/RatingService.java
+++ src/main/java/by/z1max/service/RatingService.java
@@ -1,10 +1,9 @@
 package by.z1max.service;
 
-import by.z1max.exception.DaoException;
 import by.z1max.exception.ServiceException;
 import by.z1max.model.Rating;
 
 public interface RatingService {
     Rating getById(int userId, int movieId) throws ServiceException;
-    Rating create(Rating rating) throws ServiceException, DaoException;
+    Rating create(Rating rating) throws ServiceException;
 }
