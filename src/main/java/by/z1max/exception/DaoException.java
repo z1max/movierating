707a9f6290ea2f3c7bad50ecd295b48e3680b782
z1max <forcazimaxqwe123>
2018-07-24 13:07:27 +0300

    Add constructors to exceptions

--- src/main/java/by/z1max/exception/DaoException.java
+++ src/main/java/by/z1max/exception/DaoException.java
@@ -1,6 +1,13 @@
 package by.z1max.exception;
 
 public class DaoException extends Exception {
+    public DaoException() {
+    }
+
+    public DaoException(String message) {
+        super(message);
+    }
+
     public DaoException(String message, Throwable cause) {
         super(message, cause);
     }
--- src/main/java/by/z1max/exception/ServiceException.java
+++ src/main/java/by/z1max/exception/ServiceException.java
@@ -1,9 +1,13 @@
 package by.z1max.exception;
 
 public class ServiceException extends Exception {
+    public ServiceException() {
+    }
+
     public ServiceException(String message) {
         super(message);
     }
+
     public ServiceException(String message, Exception e) {
         super(message, e);
     }
