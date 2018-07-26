0126183189b3d39ba78054cba9fb7cbe991a6a70
z1max <forcazimaxqwe123>
2018-07-26 16:32:48 +0300

    Update client validation

--- src/main/webapp/resources/js/scripts.js
+++ src/main/webapp/resources/js/scripts.js
@@ -7,31 +7,36 @@ function validate(form, usernameFlag){
     var emailPattern = new RegExp("(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|\"(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21\\x23-\\x5b\\x5d-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])*\")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21-\\x5a\\x53-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])+)\\])")
     var passwordPattern = new RegExp("^(?=.*[A-Za-z])(?=\\S+$)[A-Za-z\\d#$%^&*?!]{5,32}$");
 
+    var valid = true;
+
     if (usernameFlag) {
         if (!usernamePattern.test(username.value)) {
             showError("username");
-            return false;
+            valid =  false;
+        } else {
+            showSuccess("username")
         }
-        showSuccess("username")
     }
 
     if (!emailPattern.test(email.value.toLowerCase())){
         showError("email");
-        return false;
+        valid = false;
+    } else {
+        showSuccess("email");
     }
-    showSuccess("email");
 
     if (!passwordPattern.test(password.value)){
         showError("password");
-        return false;
+        valid =  false;
+    } else {
+        showSuccess(password);
     }
-    showSuccess(password);
-    return true;
+    return valid;
 }
 
 function showError(id){
     document.getElementById(id).style.borderColor = "red";
-    document.getElementById(id + "-error").style.display = "inline";
+    document.getElementById(id + "-error").style.display = "inline-block";
 }
 
 function showSuccess(id){
