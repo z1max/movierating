850ce0af1557af1d235dba61db654ab2961c8fd5
z1max <forcazimaxqwe123>
2018-08-02 19:52:42 +0300

    Fix username pattern

--- src/main/java/by/epam/util/ValidationUtil.java
+++ src/main/java/by/epam/util/ValidationUtil.java
@@ -3,7 +3,7 @@ package by.epam.util;
 import by.epam.exception.ServiceException;
 
 public class ValidationUtil {
-    private static final String USERNAME_PATTERN = "^[a-z0-9_-]{3,15}$";
+    private static final String USERNAME_PATTERN = "^[a-zA-Z0-9_-]{3,15}$";
     private static final String EMAIL_PATTERN = "(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|\"(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21\\x23-\\x5b\\x5d-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])*\")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21-\\x5a\\x53-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])+)\\])";
     private static final String PASSWORD_PATTERN = "^(?=.*[A-Za-z])(?=\\S+$)[A-Za-z\\d#$%^&*?!]{5,32}$";
 
