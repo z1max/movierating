ae97c20cb6dac5f8b2fe5af12a552b1e80d4ab1a
z1max <forcazimaxqwe123>
2018-08-10 21:00:55 +0300

    Add error message to UnknownCommand

--- src/main/java/by/epam/web/command/UnknownCommand.java
+++ src/main/java/by/epam/web/command/UnknownCommand.java
@@ -3,6 +3,8 @@ package by.epam.web.command;
 public class UnknownCommand extends Command {
     @Override
     public CommandResponse process() {
+
+        wrapper.setAttribute("errorMessageKey", "exception.unknownCommand");
         return CommandResponse.forwardUnknown();
     }
 }
